f8860db126defd76ee6b7c046a30bebbe651b562
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-12-09 20:36:30 -0500

    Make a more interesting-looking example, ish

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,25 @@
 extern crate alloc_system;
 
 fn main() {
-    let mut x = 0.0;
-    for i in (0..150_000) {
-        let foo = vec![i as f64; i];
-        x += sum_floats(&foo);
-    }
-
     let mut y = 0;
-    for i in (0..150_000) {
-        let foo = vec![i as i64; i];
-        y += multiply_ints(&foo);
+    for i in (0..100000) {
+        let bar = vec![i as i64; i];
+        y += multiply_ints(&bar);
     }
 
-    println!("{}, {}", x, y);
+    println!("{}", y);
 }
 
 fn sum_floats(v: &Vec<f64>) -> f64 {
     v.iter().fold(0.0, |acc, item| acc + item)
 }
 
+#[inline(never)]
 fn multiply_ints(v: &Vec<i64>) -> i64 {
+    let mut x = 0.0;
+    let foo = vec![v.len() as f64; v.len()];
+    x += sum_floats(&foo);
+    println!("{}", x);
+
     v.iter().fold(1, |acc, item| acc * item)
 }
